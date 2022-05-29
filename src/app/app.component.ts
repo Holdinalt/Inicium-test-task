badd2b7234e8f5f643861ec8081c56a0dc572df2
Holdest White <zbsholdest@mail.ru>
2022-05-29 16:47:42 +0300

    A lot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,54 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {TableHandlerService} from "./table-handler.service";
 import {RowModel} from "./row.model";
 import { MatPaginator } from '@angular/material/paginator';
 import {MatTableDataSource} from "@angular/material/table";
+
+export class PaginationInterface{
+
+  constructor(public maxRows = 0, public page = 0) {
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
+
   title = 'Inicium-test-task';
 
   rows?: RowModel[]
-  columns?: string[]
-  dataSource?: MatTableDataSource<RowModel>;
+  // columns?: string[]
+
+  paginationArr = [4 , 5 , 10]
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
+  ngOnInit(): void {
+
+  }
+
   ngAfterViewInit() {
     this.paginator.page.subscribe(x =>{
-      console.log(x)
+      this.tableHandlerService.paginateRows(x.pageIndex, x.pageSize);
     })
-    console.log(this.paginator.length)
-    // if(this.dataSource){
-    //   this.dataSource.paginator = this.paginator;
-    // }
+
+
   }
 
   constructor(public tableHandlerService: TableHandlerService) {
-    this.rows = tableHandlerService.rows;
-    this.columns = tableHandlerService.columns;
-    this.dataSource = new MatTableDataSource<RowModel>(this.rows);
-    console.log('lol')
 
+    // this.columns = tableHandlerService.columns;
+    this.tableHandlerService.rowsToShowOut?.subscribe(x =>{
+      this.rows = x;
+    })
+    this.tableHandlerService.paginateRows(0, this.paginationArr[0])
   }
+
+
 
   /** Selects all rows if they are not all selected; otherwise clear selection. */
   masterToggle() {
@@ -44,6 +58,48 @@ export class AppComponent {
     }
     this.tableHandlerService.selectAll();
   }
+
+  showEditUI(event: Event){
+
+    let child = document.createElement('div');
+    child.append('lol');
+    child.setAttribute('id', 'editUI')
+
+    let elem: HTMLElement = event.target as HTMLElement
+    if(elem.parentElement && elem.tagName === 'TD' && elem.parentElement.tagName === 'TR'){
+      console.log(elem.tagName)
+      elem = elem.parentElement;
+      elem.appendChild(
+        child
+      )
+    }
+    // console.log(elem.parentElement)
+
+
+
+
+  }
+
+  hideEditUI(event: Event){
+
+
+    let elem = document.getElementById('editUI')
+
+    if(elem){
+      elem.remove();
+      elem = document.getElementById('editUI')
+    }
+
+
+
+    // let elem: HTMLElement = event.target as HTMLElement
+    // if(elem.parentElement && elem.parentElement.className != 'table'){
+    //   elem = elem.parentElement;
+    //   elem.removeChild()
+    // }
+
+  }
+
 
   /** The label for the checkbox on the passed row */
   // checkboxLabel(row?: PeriodicElement): string {
--- src/app/table-handler.service.ts
+++ src/app/table-handler.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {RowModel} from "./row.model";
 import {LocalStorageControllerService} from "./local-storage-controller.service";
+import {BehaviorSubject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,15 @@ import {LocalStorageControllerService} from "./local-storage-controller.service"
 export class TableHandlerService{
 
   private _rows: RowModel[] = [];
-  public columns = ['select', 'name', 'email', 'phone']
+  // public columns = ['select', 'name', 'email', 'phone']
   public selectedRowsNum = 0;
+  public rowsToShow: RowModel[] = [];
+
+  public rowsToShowOut?: BehaviorSubject<RowModel[]>;
 
   constructor(private localStorageControllerService: LocalStorageControllerService) {
     this.setRows(localStorageControllerService.getTable());
+    this.rowsToShowOut = new BehaviorSubject<RowModel[]>(this.rowsToShow);
   }
 
 
@@ -25,7 +30,7 @@ export class TableHandlerService{
     this.localStorageControllerService.saveTable(this.rows)
   }
 
-  deleteSelected(): void{
+  deleteSelected(page: number, pageSize: number): void{
     let newRows: RowModel[] = []
 
     for (let row of this.rows){
@@ -38,6 +43,18 @@ export class TableHandlerService{
     this.rows = newRows;
     this.localStorageControllerService.saveTable(this.rows)
     this.selectedRowsNum = 0;
+    this.paginateRows(page, pageSize)
+    this.notifyRowsToShow()
+    console.log(this.rows)
+  }
+
+  addEmptyRow(page: number, pageSize: number){
+    let index = page * pageSize;
+    this.rows.splice(index, 0, new RowModel())
+    this.rows = this.rows
+    this.paginateRows(page, pageSize)
+
+    console.log(this.rows)
   }
 
   setRows(_rows: RowModel[]){
@@ -66,6 +83,7 @@ export class TableHandlerService{
   }
 
   toggleRow(row: RowModel){
+
     if(row.selected){
       this.selectedRowsNum--;
       row.selected = false;
@@ -73,6 +91,22 @@ export class TableHandlerService{
       this.selectedRowsNum++;
       row.selected = true;
     }
+
   }
 
+  paginateRows(page: number, maxRows: number){
+    this.rowsToShow = [];
+
+    for (let i = page * maxRows; i < (page + 1) * maxRows; i++){
+      if(i < this.rows.length){
+        this.rowsToShow.push(this.rows[i]);
+      }
+    }
+
+    this.notifyRowsToShow()
+  }
+
+  notifyRowsToShow(){
+    this.rowsToShowOut?.next(this.rowsToShow)
+  }
 }
